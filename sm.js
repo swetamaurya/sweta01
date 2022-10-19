9d63290113c3beadcd4e638c20221bae553027d7
swetamaurya <mauryasweta062@gmail.com>
2022-10-20 00:03:52 +0530

    Done Dna Done

---
+++ sm.js
@@ -0,0 +1,14 @@
+// To  check prime num.
+var num = 13;
+var count = 0;
+for (var i = 1; i <= num; i++) {
+  if (num % i == 0) {
+    count++
+  }
+}
+if (count == 1) {
+  console.log("It is a prime number");
+} else {
+  console.log("It is not a prime number");
+}
+// please find error in my code
---
+++ sw.js
@@ -0,0 +1,15 @@
+//Check if the Number is a palindrome
+var str = "Tiwari" ;
+str = "dad"
+var bag = "" ;
+
+
+for(var  i = str.length-1; i>0 ; i--) {
+  bag += str[i];
+}
+if(str == bag){
+  console.log("y");
+}else{
+  console.log("n");
+}
+//please find the error
